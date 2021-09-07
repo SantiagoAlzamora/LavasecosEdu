1a5882549f6762010a2c46dd5ac666eeb98380c3
SantiagoAlzamora <santialzamora_6@hotmail.com>
2021-09-07 19:33:25 -0300

    Creando metodos en los controladores

---
+++ src/main/java/com/lavasecos/edu/controladores/PedidoController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lavasecos.edu.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author santi
+ */
+@Controller
+@RequestMapping("/pedido")
+public class PedidoController {
+    
+    @GetMapping("")
+    public String index(){
+        return "";
+    }
+    
+    @GetMapping("/form")
+    public String cargarPedido(){
+        return "";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPedido(){
+        return "redirect:/pedido";
+    }
+    
+    @GetMapping("/delete")
+    public String eliminarPedido(){
+        return "redirect:/pedido";
+    }
+}
